2276eb35aeb42dd5a82731d85a6180e1305148b1
Gregory Leck <greg@leck.codes>
2019-10-06 20:24:43 -0400

    First commit

---
+++ SampleProject/SampleProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleProject
+//
+//  Created by Gregory Leck on 2019-10-06.
+//  Copyright © 2019 Gregory Leck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
